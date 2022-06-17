e6223ccc407bde9efe748b3590ef79ad83874073
Aleksandr Anisimov <a.e.anisimov@nexters.com>
2022-06-18 00:39:39 +0300

    added calendar class + template fix

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,13 @@
 import os
 import calendar
+from datetime import datetime
+from collections import defaultdict
 
 import bcrypt
 import markdown
-from datetime import datetime
 
 from config import STATIC_PATH
+from core.hardcode import weekends, workdays
 
 
 def get_markdown(fname):
@@ -40,3 +42,80 @@ def gen_calender():
 def get_this_month_abbr():
     date = datetime.now()
     return calendar.month_abbr[date.month]
+
+
+class Calendar:
+    """
+    jan: [{'0': {'day': '1', 'weekend': True},
+            '1': {'day': '2', 'weekend': False}}]
+    """
+    def __init__(self):
+        self.dt_now = datetime.now()
+        self.this_year = self.dt_now.year
+        self.this_date = self.dt_now.strftime('%Y.%m.%d')
+
+    def get_month_abbr(self, month=0) -> str:
+        month = month if month else self.dt_now.month
+        return calendar.month_abbr[month]
+
+    def _get_digit_days(self, month: int) -> list:
+        digit_days = calendar.monthcalendar(self.this_year, month)
+        if len(digit_days) != 6:
+            digit_days.append([0 for _ in range(7)])
+        return digit_days
+
+    def _get_cur_date_fmt(self, month: int, day: int) -> str:
+        def fill(num: int) -> str:
+            return str(num).zfill(2)
+
+        if day != '&nbsp':
+            return f'{self.this_year}.{fill(month)}.{fill(day)}'
+
+    def _is_weekend_day(self, month: int, day: int, day_index: int) -> bool:
+        cur_date = self._get_cur_date_fmt(month, day)
+        if cur_date:
+            if cur_date in workdays:
+                return False
+            if cur_date in weekends:
+                return True
+            if day_index in (5, 6):
+                return True
+
+        return False
+
+    def _is_current_month(self, month: int) -> bool:
+        if month == self.dt_now.month:
+            return True
+        return False
+
+    def _is_current_day(self, month: int, day: int) -> bool:
+        if month == self.dt_now.month and day == self.dt_now.day:
+            return True
+        return False
+
+    def gen(self):
+        result = defaultdict(dict)
+
+        for month in range(1, 13):
+            month_addr = self.get_month_abbr(month)
+            digit_days = self._get_digit_days(month)
+
+            week_result = dict()
+            for w, week in enumerate(digit_days):
+                days_result = dict()
+                for d, day in enumerate(week):
+                    days_result[d] = {
+                        'day': str(day) if day else '&nbsp',
+                        'weekend': self._is_weekend_day(month, day, d),
+                        'current_day': self._is_current_day(month, day)
+                    }
+                pass
+                week_result[w] = days_result
+            pass
+            result[month_addr] = {
+                'weeks': week_result,
+                'current_month': self._is_current_month(month)
+            }
+        pass
+
+        return result
